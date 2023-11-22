44d12ec559903a8746be44daa3ae287f3912fc8c
Miao Li <miaoli.cs@gmail.com>
2023-11-23 09:00:39 +1100

    update

--- automate_annotating/preparing_data_small.py
+++ automate_annotating/preparing_data_small.py
@@ -1,3 +1,5 @@
+import random
+
 import jsonlines
 import json
 
@@ -10,20 +12,24 @@ samples_annotated_keys = bryan_results.keys()
 
 annotation_folder = "../../HumanAnnotation/mrg_judgement"
 samples = {}
+tmp = []
 with jsonlines.open(annotation_folder + "/annotation/sampled_data.jsonl") as reader:
     for line in reader:
-        paper_id = line["paper_id"][10:]
-        if paper_id in samples_annotated_keys:
-            source_documents_new = []
-            source_documents_new.append({"document_title": "", "document_content": line["summary"]})
-            for doc in line["source_documents"]:
-                source_documents_new.append({"document_title": "", "document_content": doc})
+        tmp.append(line)
+for line in random.sample(tmp, 5):
+    paper_id = line["paper_id"][10:]
+    if paper_id in samples_annotated_keys:
+        source_documents_new = []
+        source_documents_new.append({"document_title": "", "document_content": line["summary"]})
+        for doc in line["source_documents"]:
+            source_documents_new.append({"document_title": "", "document_content": doc})
 
-            line["documents"] = source_documents_new
-            del line["paper_id"]
-            del line["summary"]
-            del line["source_documents"]
-            samples[paper_id] = line
+        line["documents"] = source_documents_new
+        del line["paper_id"]
+        del line["summary"]
+        del line["source_documents"]
+        samples[paper_id] = line
+
 print("All annotated count", len(samples))
 
 with open("gpt4_annotation_data_small.json", "w") as f:
