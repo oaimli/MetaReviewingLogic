d948baa412a74d0790a755289b3938f6d5475b58
Miao Li <miaoli.cs@gmail.com>
2024-01-29 11:02:10 +1100

    update

--- enhancing_prompting/llama2_7b_prompt_naive.py
+++ enhancing_prompting/llama2_7b_prompt_naive.py
@@ -5,26 +5,8 @@ from transformers import (
 import json
 
 
-def predict(input_text, target, model_in_use="meta-llama/Llama-2-7b-chat-hf", max_predict_length=512,
-            min_predict_length=1, do_sample=True, top_p=0.95, num_beams=1, temperature=0.7):
-    # load model and tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_in_use,
-        model_max_length=4096,
-        padding_side="right",
-        use_fast=True,
-    )
-    print("tokenizer bos", tokenizer.bos_token, tokenizer.bos_token_id)
-    print("tokenizer eos", tokenizer.eos_token, tokenizer.eos_token_id)
-    print("tokenizer pad", tokenizer.pad_token, tokenizer.pad_token_id)
-    print("tokenizer unk", tokenizer.unk_token, tokenizer.unk_token_id)
-
-    model = AutoModelForCausalLM.from_pretrained(model_in_use, trust_remote_code=True)
-    print(model.config)
-
-    # generate
-    model.to("cuda")
-    model.eval()
+def predict(model, tokenizer, input_text, max_predict_length=512, min_predict_length=1, do_sample=True, top_p=0.95, num_beams=1, temperature=0.7):
+    print(input_text)
     input_dict = tokenizer(
         [input_text],
         return_tensors="pt",
@@ -46,10 +28,30 @@ def predict(input_text, target, model_in_use="meta-llama/Llama-2-7b-chat-hf", ma
         pad_token_id=tokenizer.eos_token_id
     )
     predicted_summary = tokenizer.decode(output_ids[0][len(input_ids[0]):], skip_special_tokens=False)
-    print(predicted_summary)
+    return predicted_summary
 
 
 if __name__ == "__main__":
+    # load model and tokenizer
+    model_in_use = "meta-llama/Llama-2-7b-chat-hf"
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_in_use,
+        model_max_length=4096,
+        padding_side="right",
+        use_fast=True,
+    )
+    print("tokenizer bos", tokenizer.bos_token, tokenizer.bos_token_id)
+    print("tokenizer eos", tokenizer.eos_token, tokenizer.eos_token_id)
+    print("tokenizer pad", tokenizer.pad_token, tokenizer.pad_token_id)
+    print("tokenizer unk", tokenizer.unk_token, tokenizer.unk_token_id)
+
+    model = AutoModelForCausalLM.from_pretrained(model_in_use, trust_remote_code=True)
+    print(model.config)
+    model.to("cuda")
+    model.eval()
+
+    # load the prompt
     prompt_format = open("prompts/prompt_naive.txt").read()
 
     with open("test_data.json") as f:
@@ -61,9 +63,9 @@ if __name__ == "__main__":
         for review in sample["reviews"]:
             if review["writer"] == "official_reviewer" and review["reply_to"] == sample["paper_id"]:
                 input_texts.append(review["comment"])
-        result = predict(prompt_format.replace("{{input_documents}}", "\n".join(input_texts)), "", model_in_use="meta-llama/Llama-2-7b-chat-hf")
+        result = predict(model, tokenizer, prompt_format.replace("{{input_documents}}", "\n".join(input_texts)))
         results[key] = {"generation": result}
-        # break
+        break
 
     print(len(results))
     with open("results/generation_llama2_7b_prompt_naive.json", "w") as f:
