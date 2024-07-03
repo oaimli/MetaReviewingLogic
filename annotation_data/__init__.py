69e07f3b9795e060825ddb8ff60a250d9108ee4e
Miao Li <miaoli.cs@gmail.com>
2024-07-03 15:18:59 +0100

    update

--- annotation_data/__init__.py
+++ annotation_data/__init__.py
@@ -1,2 +1,3 @@
 # Preparation of the data for human annotation
+# The file of annotation_data_small.json contains samples to be evaluated
 # Please note that dataset here is in different splitting mode from the dataset in PeerSum
